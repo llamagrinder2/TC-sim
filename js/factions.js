3cbce93a7a4a984618ffc0bca395228902bcf586
llamagrinder2 <attila.raso@outlook.com>
2025-05-21 15:53:37 +0200

    Update factions.js

--- js/factions.js
+++ js/factions.js
@@ -1,31 +1,31 @@
 // js/factions.js
 
-// Itt definiáljuk az összes egységet, típussal és költséggel
+// Itt definiáljuk az összes egységet, típussal, költséggel és játékstatisztikákkal
 export const ALL_UNITS = {
     // Heretic Legion
-    'HPr': { name: 'Heretic Priest', cost: 80 },
-    'DCo': { name: 'Death Commando', cost: 90 },
-    'HCh': { name: 'Heretic Chorister', cost: 65 },
-    'HTr': { name: 'Heretic Trooper', cost: 30 },
-    'HLM': { name: 'Heretic Legionnaire m', cost: 40 },
-    'HLR': { name: 'Heretic Legionnaire r', cost: 40 },
-    'AHe': { name: 'Annointed Heavy Infantry', cost: 95 },
-    'WWo': { name: 'War Wolf', cost: 140 },
-    'AWi': { name: 'Artillery Witch', cost: 90 },
-    'WRe': { name: 'Wretched', cost: 25 },
+    'HPr': { name: 'Heretic Priest', cost: 80, movement: 6, ranged: 2, melee: 2, armour: 0, maxHP: 10 },
+    'DCo': { name: 'Death Commando', cost: 90, movement: 6, ranged: 2, melee: 2, armour: 0, maxHP: 10 },
+    'HCh': { name: 'Heretic Chorister', cost: 65, movement: 6, ranged: -2, melee: 2, armour: 0, maxHP: 10 },
+    'HTr': { name: 'Heretic Trooper', cost: 30, movement: 6, ranged: 0, melee: 0, armour: 0, maxHP: 10 },
+    'HLM': { name: 'Heretic Legionnaire m', cost: 40, movement: 6, ranged: 1, melee: 0, armour: 0, maxHP: 10 },
+    'HLR': { name: 'Heretic Legionnaire r', cost: 40, movement: 6, ranged: 0, melee: 1, armour: 0, maxHP: 10 },
+    'AHe': { name: 'Annointed Heavy Infantry', cost: 95, movement: 6, ranged: 1, melee: 1, armour: -2, maxHP: 10 },
+    'WWo': { name: 'War Wolf', cost: 140, movement: 8, ranged: 0, melee: 2, armour: -3, maxHP: 10 },
+    'AWi': { name: 'Artillery Witch', cost: 90, movement: 6, ranged: 0, melee: -1, armour: -1, maxHP: 10 },
+    'WRe': { name: 'Wretched', cost: 25, movement: 6, ranged: -1, melee: -1, armour: 0, maxHP: 10 },
 
     // Trench Pilgrims
-    'WPr': { name: 'War Prophet', cost: 80 },
-    'Ca': { name: 'Castigator', cost: 50 },
-    'Co': { name: 'Communicant', cost: 100 },
-    'TPi': { name: 'Trench Pilgrim', cost: 30 },
-    'MPe': { name: 'Martyr Penitent', cost: 75 },
-    'EPr': { name: 'Ecclesiastic Prisoners', cost: 55 },
-    'SNu': { name: 'Stigmatic Nuns', cost: 50 },
-    'SAn': { name: 'Shrine Anchorite', cost: 140 },
+    'WPr': { name: 'War Prophet', cost: 80, movement: 6, ranged: 2, melee: 2, armour: 0, maxHP: 10 },
+    'Ca': { name: 'Castigator', cost: 50, movement: 6, ranged: 1, melee: 1, armour: 1, maxHP: 10 },
+    'Co': { name: 'Communicant', cost: 100, movement: 6, ranged: -3, melee: 2, armour: 0, maxHP: 10 },
+    'TPi': { name: 'Trench Pilgrim', cost: 30, movement: 6, ranged: 0, melee: 0, armour: 0, maxHP: 10 },
+    'MPe': { name: 'Martyr Penitent', cost: 75, movement: 6, ranged: 0, melee: 1, armour: -1, maxHP: 10 },
+    'EPr': { name: 'Ecclesiastic Prisoners', cost: 55, movement: 6, ranged: 0, melee: 0, armour: -1, maxHP: 10 },
+    'SNu': { name: 'Stigmatic Nuns', cost: 50, movement: 8, ranged: 1, melee: 1, armour: 0, maxHP: 10 },
+    'SAn': { name: 'Shrine Anchorite', cost: 140, movement: 6, ranged: 0, melee: 2, armour: -3, maxHP: 10 },
 };
 
-// Itt definiáljuk a frakciókat, és a hozzájuk tartozó egységek rövidítéseit
+// ... (FACTIONS és factionNames része változatlan)
 export const FACTIONS = {
     'Heretic Legion': {
         units: ['HPr', 'DCo', 'HCh', 'HTr', 'HLM', 'HLR', 'AHe', 'WWo', 'AWi', 'WRe'],
@@ -33,9 +33,6 @@ export const FACTIONS = {
     'Trench Pilgrims': {
         units: ['WPr', 'Ca', 'Co', 'TPi', 'MPe', 'EPr', 'SNu', 'SAn'],
     },
-    // Ide jönnek majd a többi frakció, ha hozzáadjuk őket
-    // 'The Iron Sultanate': { units: [] },
-    // ...
 };
 
 export const factionNames = Object.keys(FACTIONS);
